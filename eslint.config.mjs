d9ebd866272ad29d20a64e43349f9f94223896c6
Gaurav Amatya <gma.gaurav@gmail.com>
2025-06-07 23:44:08 -0500

    refactor: clean up eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,17 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from 'eslint-plugin-react';
-import reactPlugin from "eslint-plugin-react";
+import js from "@eslint/js"
+import pluginReact from "eslint-plugin-react"
+// import reactPlugin from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  js.configs.recommended,
-  {
-    files: ["**/*.jsx", "**/*.tsx"],
-    plugins: {
-      react: reactPlugin // Use correct import
-    },
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off"
-    },
-    settings: {
-      react: {
-        version: "detect" // Automatically detect React version
-      }
-    }
-  }
-]);
+])
