22e52b36c11d5f13bc6e1443553953df696f9019
Gaurav Amatya <gma.gaurav@gmail.com>
2025-07-26 13:46:18 -0500

    feat: integrate Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Mona_Sans } from "next/font/google"
 import React from "react"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 import { Toaster } from "sonner"
 
 const monaSans = Mona_Sans({
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
